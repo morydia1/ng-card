8d4f2149b1d4884b365dbdafce66c0d9d5a8e5c9
Mory DIA <dia.mory@heb.com>
2024-05-06 23:25:02 -0500

    Initil Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'card';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat today'
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'nature',
+      content: 'I saw this neat today'
+    },
+    {
+      title: 'Mory Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'Mory',
+      content: 'I saw you biking today'
+    },
+    {
+      title: 'Mory Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'Mory',
+      content: 'I saw you biking today'
+    }
+  ]  
 }
